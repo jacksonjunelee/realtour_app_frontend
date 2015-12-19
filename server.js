d44c23cf933f23930be727bc6c5761227b3ad6cd
Jackson <jlee@Jacksons-MacBook-Pro.local>
2015-12-19 14:46:16 -0500

    move code to new frontend repo

---
+++ server.js
@@ -0,0 +1,16 @@
+// load express package and create app
+var express = require("express")
+	, app = express()
+	, path = require("path");
+
+//set static assets
+app.use("/css", express.static(__dirname + '/css'))
+app.use("/static", express.static(__dirname + '/static'))
+
+// set homepage
+app.get( '/', function( req, res ) {
+	res.sendFile( path.join( __dirname + "/landing_page.html" ) );
+});
+
+app.listen( 1337 );
+console.log( "magical shit happening on 1337" );
